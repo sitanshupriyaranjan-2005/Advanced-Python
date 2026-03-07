dd2e4971b2c8d0aba67314c825984b00156699d7
sitanshupriyaranjan-2005 <sitanshupriyaranjan20@gmail.com>
2026-03-07 17:29:44 +0530

    Add files via upload

---
+++ practiceqs/count.py
@@ -0,0 +1,4 @@
+grade=["A","B","C","D","A","A"]
+print(grade.count("A"))
+grade.sort()
+print(grade)
---
+++ practiceqs/movies.py
@@ -0,0 +1,8 @@
+movies=[]
+mov1=input("Enter the name of the movie: ")
+mov2=input("Enter the name of the movie: ")
+mov3=input("Enter the name of the movie: ")
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+print(movies)
